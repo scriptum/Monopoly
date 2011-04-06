949dc53f740ab0e5aa3d56b294e79833dea6311b
azazello190 <azaz86@bk.ru>
2011-04-07 00:01:12 +0400

    cvetomuzika

--- game/board.lua
+++ game/board.lua
@@ -98,7 +98,7 @@ render.company = function(s)
   local sx = (s2 - cell_padding*2)/16
   local sy = (s1 - cell_padding*2)/16
   local sx2 = (s2 + a)/16
-  G.setColor(255, 0, 0, 70)
+  if rules_company.owner then G.setColor(rules_player_colors[rules_company.owner]) end
   if s.side == 1 then
     G.draw(fuzzy, x, y, 0, sx, sy)
   elseif s.side == 2 then
@@ -313,6 +313,7 @@ gogo = function(s)
  if buf.pos > max then buf.pos = buf.pos - max end
  local x, y = getplayerxy(buf.pos, buf.k)
  buf:stop('main'):animate({x=x,y=y})
+ if not rules_company[__i].owner then rules_company.owner = buf.k end
  if ds1 ~= ds2 then
   __i = __i + 1
   if __i > 5 then __i = 1 end
