7262fd2849b1fdf61466c9675ad3f12f3d63148b
azazello190 <azaz86@bk.ru>
2011-04-07 18:29:12 +0400

    добавили в ИНИТ замок

--- data/init.lua
+++ data/init.lua
@@ -1,6 +1,6 @@
 board_background = G.newImage('data/gfx/background.jpg')
 sep = G.newImage('data/gfx/separator.png')
-logo = G.newImage('data/gfx/logos/mcdonalds-logo.png')
+zamok = G.newImage('data/gfx/Lock-icon.png')
 fuzzy = G.newImage('data/gfx/fuzzy.png')
 dice = {}
 for i = 1,6 do
