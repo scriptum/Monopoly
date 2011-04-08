83d4e575ab81e60256fdcb87a68d8c79af7d9c43
azazello190 <azaz86@bk.ru>
2011-04-09 02:48:32 +0400

    speed=0.5 при переходе в тюрьму

--- rules/classic.lua
+++ rules/classic.lua
@@ -62,7 +62,7 @@ end
 action_jail = function(player)
  player.pos = 13
  local x, y = getplayerxy(13, player.k)
- player:stop('main'):animate({x=x}):animate({y=y})
+ player:stop('main'):animate({x=x}, speed=0.5):animate({y=y}, speed=0.5)
 end
 
 --группы, одна группа означает как монополию так и просто клетки одного типа
