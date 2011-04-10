b1f11872b603093aa4617c5fe7a8e6e80b567e2d
rpg <rpg@localhost.(none)>
2011-04-11 03:43:09 +0400

    Поменял шрифт на растр - стало быстрее

--- game/board.lua
+++ game/board.lua
@@ -245,7 +245,7 @@ render.chance = function(s)
   G.setColor(0,0,0)
   G.setFont(console)
   G.fontSize = 15
-  Gprintf(rules_company[s.num].name, x, y, s2+10, 'center')
+  Gprintf(rules_company[s.num].name, x, y, s2+6, 'center')
   --G.rectangle('line', x,y,s2- cell_padding * 2,12)
 end
 
