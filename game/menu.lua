10bc3c2b0c31a4d251f2d45df52b47fb15d89121
azazello190 <azaz86@bk.ru>
2011-05-08 21:03:07 +0400

    добавлены языки

--- game/menu.lua
+++ game/menu.lua
@@ -1,9 +1,9 @@
 --меню для игрока
 playermenu = E:new(screen)
 menuplayer = E:new(playermenu):menu({
-	{text = 'Buy company', action = human_buy_company},
-	{text = 'Auction', action = human_auction},
-	{text = 'Mortgage', action = function() 
+	{text = l.buy_company, action = human_buy_company},
+	{text = l.auction, action = human_auction},
+	{text = l.mortgage, action = function() 
 		for k, v in pairs(companys._child) do 
 			local c = rules_company[v.num]
 			if not(c.owner == player._child[current_player] and c.level > 0) then
@@ -12,7 +12,7 @@ menuplayer = E:new(playermenu):menu({
 		end 
 		gui_mortgage_done:menutoggle(menuplayer)
 	end},
-	{text = 'Shares', action = function() 
+	{text = l.shares, action = function() 
 		for k, v in pairs(companys._child) do 
 			local c = rules_company[v.num]
 			if not(c.owner == player._child[current_player] and c.level > 1) or (c.group == 'oil' or c.group == 'bank') then
@@ -21,7 +21,7 @@ menuplayer = E:new(playermenu):menu({
 		end 
 		gui_shares_done:menutoggle(menuplayer)
 	end},
-	{text = 'Trade', action = function() 
+	{text = l.trade, action = function() 
 		for k, v in pairs(companys._child) do 
 			local c = rules_company[v.num]
 			if not(c.owner == player._child[current_player] and c.level == 0) then
@@ -31,27 +31,27 @@ menuplayer = E:new(playermenu):menu({
 		gui_trade_done:menutoggle(menuplayer)
 	end}
 }):hide()
-end_move = E:new(menuplayer):move(272, 120):button('End turn', turn):hide()
+end_move = E:new(menuplayer):move(272, 120):button(l.end_turn, turn):hide()
 
-game_ower = E:new(menuplayer):move(272, 194):button('Surrender', game_ower):hide()
+game_ower = E:new(menuplayer):move(272, 194):button(l.surrender, game_ower):hide()
 
-gui_pay50k = E:new(menuplayer):move(272, 194):button('Pay $ 50 K', pay50k):hide()
+gui_pay50k = E:new(menuplayer):move(272, 194):button(l.pay_50_K, pay50k):hide()
 
-gui_shares_done = E:new(playermenu):move(272, 194):button('Done', function() 
+gui_shares_done = E:new(playermenu):move(272, 194):button(l.done, function() 
 	gui_shares_done:menutoggle(menuplayer)
 	for k, v in pairs(companys._child) do
 		v:animate({all_alpha = 0})
 	end 
 end):hide()
 
-gui_mortgage_done = E:new(playermenu):move(272, 194):button('Done', function() 
+gui_mortgage_done = E:new(playermenu):move(272, 194):button(l.done, function() 
 	gui_mortgage_done:menutoggle(menuplayer)
 	for k, v in pairs(companys._child) do
 		v:animate({all_alpha = 0})
 	end 
 end):hide()
 
-gui_trade_done = E:new(playermenu):move(272, 194):button('Done', function() 
+gui_trade_done = E:new(playermenu):move(272, 194):button(l.done, function() 
 	gui_trade_done:menutoggle(menuplayer)
 	for k, v in pairs(companys._child) do
 		v:animate({all_alpha = 0})
@@ -67,9 +67,9 @@ for _, v in pairs(modes) do
 	table.insert(dislay_modes, v.width .. 'x' .. v.height)
 end
 
-local screenlist = E:new(menuvsettings):move(200, 150):list('Screen resolution', dislay_modes, dislay_modes, {'gameoptions', 'mode'})
-local screenmode = E:new(menuvsettings):move(200, 200):list('Screen mode', {true, false}, {'fullscreen', 'windowed'}, {'gameoptions', 'fullscreen'})
-local apply = E:new(menuvsettings):move(130, 400):button('Apply', function(s) 
+local screenlist = E:new(menuvsettings):move(200, 150):list(l.screen_resolution, dislay_modes, dislay_modes, {'gameoptions', 'mode'})
+local screenmode = E:new(menuvsettings):move(200, 200):list(l.screen_mode, {true, false}, {l.fullscreen, l.windowed}, {'gameoptions', 'fullscreen'})
+local apply = E:new(menuvsettings):move(130, 400):button(l.apply, function(s) 
 	local p = screenlist._pos
 	local mode = G.getWidth() .. 'x' .. G.getHeight()
 	local full = screenmode._vars[screenmode._pos]
@@ -79,29 +79,29 @@ local apply = E:new(menuvsettings):move(130, 400):button('Apply', function(s)
 		screen_scale, x_screen_scale, y_screen_scale = getScale() --rescale screen
 	end
 end)
-E:new(menuvsettings):move(200, 250):slider('Sound volume', 0, 100, {'gameoptions', 'volume'}, function(v)A.setVolume(v/100)end)
+E:new(menuvsettings):move(200, 250):slider(l.sound_volume, 0, 100, {'gameoptions', 'volume'}, function(v)A.setVolume(v/100)end)
 
 
 local function mainsettings() menumain:menutoggle(menuvsettings) end
 local function mainsingle() menumain:menutoggle(menusingle) end
-E:new(menuvsettings):move(414, 400):button('Back', mainsettings)
+E:new(menuvsettings):move(414, 400):button(l.back, mainsettings)
 
 menumain = E:new(gamemenu):menu({
-	{text = 'Singleplayer', action = mainsingle},
-	{text = 'Multiplayer', action = nil},
+	{text = l.singleplayer, action = mainsingle},
+	{text = l.multiplayer, action = nil},
 	--{text = 'Audio settings', action = nil},
-	{text = 'Settings', action = mainsettings},
-	{text = 'Quit', action = function() love.event.push('q') end}
+	{text = l.settings, action = mainsettings},
+	{text = l.quit, action = function() love.event.push('q') end}
 })
 
 menusingle = E:new(gamemenu):hide()
 
-local singleplayer_options = {'Human', 'Computer', 'Empty'}
+local singleplayer_options = {l.human, l.computer, l.empty}
 for i = 1, 5 do
-	E:new(menusingle):move(200, 100+i*50):list('player', singleplayer_options, singleplayer_options, {'initplayers', i})
+	E:new(menusingle):move(200, 100+i*50):list(l.player, singleplayer_options, singleplayer_options, {'initplayers', i})
 	E:new(menusingle):image(rules_player_images[i]):move(280, 100+i*50):scale(40/64, 40/64)
 end
-E:new(menusingle):move(130, 400):button('Start', new_game)
-E:new(menusingle):move(414, 400):button('Back', mainsingle)
+E:new(menusingle):move(130, 400):button(l.start, new_game)
+E:new(menusingle):move(414, 400):button(l.back, mainsingle)
 
 
