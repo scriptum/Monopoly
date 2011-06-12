5b64345853c81f756831eb89dfa8378b42c4d585
RPG <rpg@scriptumplus.ru>
2011-06-12 07:47:33 +0400

    printf

--- game/scaled_font.lua
+++ game/scaled_font.lua
@@ -12,7 +12,8 @@ end
 Gprintf = function(text, x, y, limit, align)
   local s = 1/screen_scale
   S.scale(s)
-  
+  --~ S.setLineWidth(3)
+  --~ S.rectangle(math.floor((x or 0)/s), math.floor((y or 0)/s), limit/s,limit/s)
   S.printf(text, math.floor((x or 0)/s), math.floor((y or 0)/s), (limit or 0)/s, align)
   S.scale(screen_scale)
 end
