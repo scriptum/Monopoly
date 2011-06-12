b4ffa396b3fb69bfdb0cba53086087729cb5cd35
RPG <rpg@scriptumplus.ru>
2011-06-12 06:34:31 +0400

    fixes

--- game/scaled_font.lua
+++ game/scaled_font.lua
@@ -1,7 +1,7 @@
 --~ G.oldprintf = G.printf
 G.fontSize = 10
 G.fontSizeOrig = 24
-Gprintf = function(text, x, y, limit, align)
+Gprint = function(text, x, y, limit, align)
   local s = 1/screen_scale
   S.scale(s)
   
@@ -9,4 +9,10 @@ Gprintf = function(text, x, y, limit, align)
   S.scale(screen_scale)
 end
 
-Gprint = Gprintf
+Gprintf = function(text, x, y, limit, align)
+  local s = 1/screen_scale
+  S.scale(s)
+  
+  S.printf(text, math.floor((x or 0)/s), math.floor((y or 0)/s), (limit or 0)/s, align)
+  S.scale(screen_scale)
+end
