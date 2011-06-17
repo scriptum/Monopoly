504fabe5d78d38ab0a1632a181fd79361a104191
azazello190 <azaz86@bk.ru>
2011-06-17 18:14:55 +0400

    тест сервера баг-версия-1

--- game/client.lua
+++ game/client.lua
@@ -2,6 +2,62 @@
 S.newThread('game/server.lua')
 print('client')
 local lasttime = 0
+
+getplayerxy = function(n, k)
+  side = companys._child[n].side
+  x, y = get_xy(companys._child[n].pos, side)
+  if side == 1 then
+    x = x + math.cos(k*math.pi)*14 + 8
+    y = y + k*12 - 3
+  elseif side == 2 then
+    x = x + k*14 - 12
+    y = y + math.cos(k*math.pi)*12 + 12
+  elseif side == 3 then
+    x = x + math.cos(k*math.pi)*12 + 8
+    y = y + k*12 - 12
+  elseif side == 4 then
+    x = x + k*14 -15
+    y = y + math.cos(k*math.pi)*12 + 12
+  else
+    x = x + k*14 -15
+    y = y + math.cos(k*math.pi*0.5)*30 + 24
+  end
+  return x + cell_padding, y + cell_padding
+end
+
+move = function(num, x)
+  pl = player._child[num]
+  if pl.jail == 0 then
+    local pl_x, pl_y
+    local last_cell = pl.pos
+    local last_i = 1
+    -- добавляем все углы, по которым проходим
+    for i=1, x do
+      pl.pos = pl.pos + 1
+      if pl.pos > max then
+	pl.pos = pl.pos-max
+      end
+      if table.find(angles, pl.pos) > 0 then
+	pl_x, pl_y = getplayerxy(pl.pos, pl.k)
+	pl:animate({x=pl_x, y=pl_y},{speed=(i + 1 - last_i)/5})
+	last_cell = pl.pos
+	last_i = i
+      end
+    end
+    if pl.pos ~= last_cell then
+      pl_x, pl_y = getplayerxy(pl.pos, pl.k)
+      pl:animate({x=pl_x, y=pl_y},{speed=(x + 1 - last_i)/5})
+    end
+  end
+end
+
+player = E:new(board)
+
+for k = 1, 5 do
+  E:new(player)
+  :draw(player_draw):set{k=k, jail=0, pos=1}  
+end
+
 table.insert(lquery_hooks, function()
 	--проверка каждый 20 мс - как раз время накопления стека ТСР
 	if time - lasttime > 0.02 then 
--- game/server.lua
+++ game/server.lua
@@ -6,25 +6,32 @@ local send = S.send
 local delay = S.delay
 local players = {} --на сервере свои плееры
 local i
+local cell_count = field_width * 2 + field_height * 2
+angles = {1, field_width + 2, field_width + field_height + 3, field_width * 2 + field_height + 4}
+
+send('max = '..cell_count, 'g')
+send('angles = {1, '..field_width..' + 2, '..field_width..' + '..field_height..' + 3, '..field_width..' * 2 + '..field_height..' + 4}', 'g')
+
 --создаем игроков
 for i = 1, 5 do
 	table.insert(players, {
 		pos = 1, 
 		jail = 0, 
-		ingame = false, 
+		ingame = true, 
 		cash = 1500, 
 		name = "", --имя
 		address = "", --айпишник (ну в теории он будет)
 		uid = 0 --серверу нужно будет как-то понять, от какого игрока пришло сообщение
 	})
+	send('player._child['..i..'].ingame = true move('..i..', 1)', 'g')
 end
 --читсло клеток всего
-local cell_count = field_width * 2 + field_height * 2
+
 local companys = {}
 for i = 1, cell_count do
 	table.insert(companys, {})
 end
-send('start_new_game() menumain:hide()', 'g') -- запуск игры
+--send('start_new_game() menumain:hide()', 'g') -- запуск игры
 local done = false
 while done == false do
 	--send('burn:animate({a = '..math.random(50, 100)..'})', 'g')
