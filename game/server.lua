062507d24be4134b65d6db2968ad8e6390c963d6
RPG <rpg@scriptumplus.ru>
2011-06-17 19:29:55 +0400

    Усправил плавное появление игроков

--- game/server.lua
+++ game/server.lua
@@ -5,14 +5,14 @@ local S = scrupp
 local send = S.send
 local delay = S.delay
 local players = {} --на сервере свои плееры
-local i
+--читсло клеток всего
 local cell_count = field_width * 2 + field_height * 2 + 4
-angles = {1, field_width + 2, field_width + field_height + 3, field_width * 2 + field_height + 4}
 
-send('max = '..cell_count, 'g')
-send('angles = {1, '..field_width..' + 2, '..field_width..' + '..field_height..' + 3, '..field_width..' * 2 + '..field_height..' + 4}', 'g')
+send('max = '..cell_count .. ' angles = {1, '..field_width..' + 2, '..field_width..' + '..field_height..' + 3, '..field_width..' * 2 + '..field_height..' + 4}', 'g')
 
+local msg = ''
 --создаем игроков
+local i
 for i = 1, 5 do
 	table.insert(players, {
 		pos = 1, 
@@ -23,15 +23,16 @@ for i = 1, 5 do
 		address = "", --айпишник (ну в теории он будет)
 		uid = 0 --серверу нужно будет как-то понять, от какого игрока пришло сообщение
 	})
-	send('player._child['..i..'].ingame = true', 'g')
+	msg = msg .. 'player._child['..i..'].ingame = true '
 end
---читсло клеток всего
+send(msg, 'g')
+
 
 local companys = {}
 for i = 1, cell_count do
 	table.insert(companys, {})
 end
---send('start_new_game() menumain:hide()', 'g') -- запуск игры
+
 local done = false
 while done == false do
 	--send('burn:animate({a = '..math.random(50, 100)..'})', 'g')
