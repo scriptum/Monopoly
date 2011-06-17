585a77ebdaf20225fa267683ce7ed75a0c10c708
RPG <rpg@scriptumplus.ru>
2011-06-17 17:09:37 +0400

    Выключил game

--- game/init.lua
+++ game/init.lua
@@ -3,6 +3,6 @@ require('game.scaled_font')
 initplayers = {'Computer', 'Computer', 'Empty', 'Empty', 'Empty'}
 require('game.board')
 require('game.client')
-require('game.game')
+--require('game.game')
 require('game.menu')
 require('game.quit')
